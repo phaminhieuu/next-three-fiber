4f37f735c928980b29dd637a0168df3398476172
phamminhhieu85 <minhhieupham851999@gmail.com>
2022-07-02 18:46:41 +0700

    add images

--- constants/link.js
+++ constants/link.js
@@ -6,43 +6,43 @@ const links = [
   },
   {
     name: "Material",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/material.png",
     path: "/material",
   },
   {
     name: "3D Text",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/3dtext.png",
     path: "/3dtext",
   },
   {
     name: "Light",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/lights.png",
     path: "/lights",
   },
   {
     name: "Shadow",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/shadows.png",
     path: "/shadow",
   },
   {
     name: "Haunted House",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/haunted-house.png",
     path: "/hauntedhouse",
   },
   {
     name: "Particle",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/particle.png",
     path: "/particle",
   },
 
   {
     name: "Galaxy generator",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/galaxy-generator.png",
     path: "/galaxy",
   },
   {
     name: "Raycaster",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/raycaster.png",
     path: "/raycaster",
   },
   {
@@ -50,28 +50,28 @@ const links = [
     image: "/images/thumbnail/texture.png",
     path: "/scroll-base-animation",
   },
-  { name: "Physics", image: "/images/thumbnail/texture.png", path: "/physics" },
+  { name: "Physics", image: "/images/thumbnail/physics.png", path: "/physics" },
   {
     name: "Imported Models",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/imported-model.png",
     path: "/importmodel",
   },
   {
     name: "Realistic Render",
-    image: "/images/thumbnail/texture.png",
+    image: "/images/thumbnail/realistic-render.png",
     path: "/realisticrender",
   },
-  { name: "Shaders", image: "/images/thumbnail/texture.png", path: "/shaders" },
+  { name: "Shaders", image: "/images/thumbnail/shaders.png", path: "/shaders" },
   {
     name: "Shader Partterns",
     image: "/images/thumbnail/texture.png",
     path: "/shaderpartterns",
   },
-  {
-    name: "Morph Object",
-    image: "/images/thumbnail/texture.png",
-    path: "/morphobject",
-  },
+  // {
+  //   name: "Morph Object",
+  //   image: "/images/thumbnail/texture.png",
+  //   path: "/morphobject",
+  // },
 ];
 
 export default links;
